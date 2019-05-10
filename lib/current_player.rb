dc12a0a826cbb8e99c60080bb9574db932e21a0a
Beto Dute <betodute@gmail.com>
2019-05-10 01:40:34 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 def turn_count(board)
   counter = 0
   board.each do |xoro|
@@ -8,6 +9,24 @@ def turn_count(board)
   return counter
 end
 
+=======
+
+
+def turn_count(board)
+  counter = 0
+  board.each do |xoro|
+    if xoro == "X"
+      counter += 1
+    elsif xoro == "O"
+      counter += 1
+    else
+      turn_count(board)
+    end
+  return counter
+end
+
+
+>>>>>>> eb4252fdf9b7236e66021c9e15867f4bcb825596
 def current_player(board)
   counter = turn_count(board)
     if counter % 2 == 0 
@@ -16,3 +35,7 @@ def current_player(board)
       return "O"
     end
   end
+<<<<<<< HEAD
+=======
+end    
+>>>>>>> eb4252fdf9b7236e66021c9e15867f4bcb825596
